06e3e9bdcb4238d2a70f9c55ea7b7140625cc663
yatuoli123 <218794rh@gmail.com>
2026-08-22 17:40:00 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/com/shizux/app/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.shizux.app
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+import android.graphics.Color
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tv = TextView(this)
+        tv.text = "ShizuX OK"
+        tv.setTextColor(Color.WHITE)
+        tv.setBackgroundColor(Color.parseColor("#7C4DFF"))
+        tv.textSize = 24f
+        setContentView(tv)
+    }
+}
