5ba0c1e13d561c28b7568b75c53b97ba9b2f476c
yatuoli123 <218794rh@gmail.com>
2026-08-22 17:57:39 +0800

    Update MainActivity.kt

--- app/src/main/java/com/shizux/app/MainActivity.kt
+++ app/src/main/java/com/shizux/app/MainActivity.kt
@@ -3,18 +3,22 @@ package com.shizux.app
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
-import android.view.ViewGroup
+import android.widget.Toast
 import android.graphics.Color
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 双保险：代码里也隐藏标题栏
+        requestWindowFeature(android.view.Window.FEATURE_NO_TITLE)
+
         val btn = Button(this)
         btn.text = "点我"
-        btn.setOnClickListener {
-            android.widget.Toast.makeText(this, "运行成功！", android.widget.Toast.LENGTH_LONG).show()
-        }
         btn.setBackgroundColor(Color.parseColor("#7C4DFF"))
+        btn.setOnClickListener {
+            Toast.makeText(this, "运行成功！", Toast.LENGTH_LONG).show()
+        }
         setContentView(btn)
     }
 }
