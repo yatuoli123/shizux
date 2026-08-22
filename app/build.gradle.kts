49c49f8bd20d3284c681c8ab5c50b63e2e8d0e57
yatuoli123 <218794rh@gmail.com>
2026-08-22 16:59:00 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -39,3 +39,9 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.activity:activity-compose:1.9.1")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
