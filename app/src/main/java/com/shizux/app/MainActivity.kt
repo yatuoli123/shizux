e81a9571e8e65815b9874dc6ad43670c6791b4d9
yatuoli123 <218794rh@gmail.com>
2026-08-22 17:48:11 +0800

    Update MainActivity.kt

--- app/src/main/java/com/shizux/app/MainActivity.kt
+++ app/src/main/java/com/shizux/app/MainActivity.kt
@@ -2,17 +2,19 @@ package com.shizux.app
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.view.ViewGroup
 import android.graphics.Color
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tv = TextView(this)
-        tv.text = "ShizuX OK"
-        tv.setTextColor(Color.WHITE)
-        tv.setBackgroundColor(Color.parseColor("#7C4DFF"))
-        tv.textSize = 24f
-        setContentView(tv)
+        val btn = Button(this)
+        btn.text = "点我"
+        btn.setOnClickListener {
+            android.widget.Toast.makeText(this, "运行成功！", android.widget.Toast.LENGTH_LONG).show()
+        }
+        btn.setBackgroundColor(Color.parseColor("#7C4DFF"))
+        setContentView(btn)
     }
 }
